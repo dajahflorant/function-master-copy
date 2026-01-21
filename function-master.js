ec7d94757d01b3d778ae74ee8bd734a9c823a103
Da'jah Florant <dajahmccallon5@gmail.com>
2026-01-21 16:34:42 -0600

    describe work

--- function-master.js
+++ function-master.js
@@ -3,8 +3,9 @@
 //////////////////////////////////////////////////////////////////////
 
 
-function objectValues(object) {
-
+function objectValues(object) { // num 13
+//   Should take an object and return its values in an array
+// return statement
     return Object.values(object);
   
 } 
@@ -12,121 +13,254 @@ function objectValues(object) {
 //////////////////////////////////////////////////////////////////////
 // Function 2 - Keys to String ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-function keysToString(object) {
-
+ 
+function keysToString(object) { // num 14
+// Should take an object and return all its keys in a string each separated with a space"
+// use join method to return keys
+return Object.keys(object).join(' ');
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 3 - Values to String /////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function valuesToString(object) {
-    
+function valuesToString(object) { // num 15
+    // Should take an object and return all its string values in a string each separated with a space"
+    // create loop
+    let result = '';
+    // create loop
+    for (const key in object) {
+
+        // type of value secured
+        if (typeof object [key] === 'string') {
+            // if statement
+            if ( result.length > 0 ) {
+                // result
+                result += ' ';
+
+            }
+            // result
+                result += object[key];
+            }
+        }
+        // return result
+        return result;
 }
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 4 - Array or Object //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function arrayOrObject(collection) {
-    
+function arrayOrObject(collection) { // num 16
+    // Should take one argument and return 'array' if its an array and 'object' if its an object"
+    // array.isarray method to see if true or false
+    if(Array.isArray(collection)){
+        // return array
+        return 'array';
+        // else statement
+    }else{
+        // return object 
+    return 'object';
+    }
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 5 - Capitalize Word //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function capitalizeWord(string) {
-    
+function capitalizeWord(string) { // num 17
+ // Should take a string of one word, and return the word with its first letter capitalized"
+ // acces the index
+ return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 6 - Capitalize All Words /////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function capitalizeAllWords(string) {
-    
+function capitalizeAllWords(string) { // num 18
+    //  Should take a string of words and return a string with all the words capitalized 
+    // capitalize words and join them back in single string
+    const words = string.split(' ');
+    // create loop
+    for (let i = 0; i < words.length; i++) {
+        // .slice method
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    // return join
+    return words.join(' ');
 }
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 7 - Welcome Message //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function welcomeMessage(object) {
+function welcomeMessage(object) { // num 19
+// Should take an object with a name property and return 'Welcome <Name>!'"
+const name = object.name;
+const capitalizedName = toUpperCase() + name.slice(1);
+return `Welcome ${capitalizedName}!`;
+
 
 }
+
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 8 - Profile Info /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function profileInfo(object) {
+function profileInfo(object) { // num 20
+// Should take an object with a name an a species and return '<Name> is a <Species>'"
 
+
+
+
+
+
+
+
+
+//return `${object.name} is a  ${object.species}`;
 }
+//return object.name + " is a " + object.species;
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 9 - Maybe Noises /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function maybeNoises(object) {
-
+function maybeNoises(object) { // num 21
+//  Should take an object, if this object has a noises array return them as a string separated by a space, if there are no noises return 'there are no noises'",
+if (object && object.noises && Array.isArray(object.noises) && object.noises.length > 0) {
+    // return & join
+    return object.noises.join(' ');
+    // else
+} else {
+    // return
+return "there are no noises";
 }
+}
+
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 10 - Has Words ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function hasWord(string, word) {
-
+function hasWord(string, word) { // num 22
+//  Should take a string of words and a word and return true if <word> is in <string of words>, otherwise return false."
+// use include method to return true
+return string.includes(word);
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 11 - Add Friend //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function addFriend (name, object) {
-
+function addFriend (name, object) { // num 23
+// Should take a name and an object and add the name to the object's friends array then return the object
+// create if statemnt 
+if (!object.friends) {
+    // emotyy array
+    object.friends = [];
+}
+// push methid
+object.friends.push(name);
+// return object
+return object;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 12 - Is Friend ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function isFriend(name, object) {
-
+function isFriend(name, object) { // num 24
+// Should take a name and an object and return true if <name> is a friend of <object> and false otherwise"
+// access friend list to make sure if its true or false using include
+// declar if statemnt with obj and array method
+if (object && object.friends && Array.isArray(object.friends)) {
+    // return statement
+return object.friends.includes(name);
 }
+// return 
+return false;
+}
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 13 - Non-Friends /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function nonFriends(name, array) {
-
+function nonFriends(name, array) { // num 25
+//  Should take a name and a list of people, and return a list of all the names that <name> is not friends with
+const nonFriends = [];
+// for statemnt
+for (let i = 0; i < array.length; i++) {
+    // if statmen
+    if (array[i]!== name) {
+        // push method
+        nonFriendsList.push(array[i]);
+    }
 }
+// return
+return nonFriendsList;
+}
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 14 - Update Object ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function updateObject(object, key, value) {
+function updateObject(object, key, value) { // num26
+// Should take an object, a key and a value. Should update the property <key> on <object> with new <value>. If <key> does not exist on <object> create it.
+object[key] = value;
+// return
+return object;
 
 }
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 15 - Remove Properties ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function removeProperties(object, array) {
-
+function removeProperties(object, array) { // num 27
+// Should take an object and an array of strings. Should remove any properties on <object> that are listed in <array>"
+// cretate loop
+for (let i = 0; i < array.length; i++) {
+    // delete
+delete object[array[i]];
 }
+}
+
+
 
 //////////////////////////////////////////////////////////////////////
 // Function 16 - Dedup ///////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function dedup(array) {
-
+function dedup(array) { // num 28
+// Should take an array and return an array with all the duplicates removed"
+return Array.from(new Set(array));
 }
 
 //////////////////////////////////////////////////////////////////////
--- test-master.js
+++ test-master.js
@@ -1,16 +1,16 @@
 (function(){
 
-    QUnit.test( "Our First Test", function( assert ) {
+    QUnit.test( "Our First Test", function( assert ) { // num 1
       var value = "hello tests";
       var some_number = 484;
   
       //                     ┌ Change this to what it should be
-      assert.equal( value, '???');
+      assert.equal( value, 'hello tests'); // print hello tests here
       //                           ┌ Change this to what it should be
-      assert.equal( some_number, "???");
+      assert.equal( some_number, "484"); // print 484 here
     });
   
-    QUnit.test("Functions can access/modify variables in parent scope.", function(assert){
+    QUnit.test("Functions can access/modify variables in parent scope.", function(assert){ // num 2
       var outside_the_function = null;
 
       function yay(){
@@ -20,22 +20,22 @@
   
       yay();
   
-      assert.equal(outside_the_function, "???");
+      assert.equal(outside_the_function, "can you see me?"); // print can you see me here?
     });
   
-    QUnit.test("Function Parameters become scoped to the function.", function(assert){
+    QUnit.test("Function Parameters become scoped to the function.", function(assert){ // num 3
   
       function yay(param){
-        assert.equal(param, "???");
+        assert.equal(param, "a fine kettle of fish"); // print a fine kettle of fish here
       }
   
       yay("a fine kettle of fish");
     });
   
-    QUnit.test("A functions local scope is not available in an outer scope.", function(assert){
+    QUnit.test("A functions local scope is not available in an outer scope.", function(assert){ // num 4
       function yay(){
         var kix = "kid tested mother approved";
-        assert.equal(kix, "???");
+        assert.equal(kix, "kid tested mother approved"); // print kid tested mother approved here 
       }
       yay();
       
@@ -49,10 +49,10 @@
       } else {
         has_kix = "i prefer cheerios";
       }
-      assert.equal(has_kix, "???");
+      assert.equal(has_kix, "i prefer cheerios"); // print i prefer cheerios here
     });
   
-    QUnit.test("Functions don't have access to eachothers scope", function(assert){
+    QUnit.test("Functions don't have access to eachothers scope", function(assert){ // num 5
       function yay(){
         var from_yay = "i'm inside yay;";
       }
@@ -62,28 +62,28 @@
         if(this.from_yay !== undefined){
           in_foo = this.from_yay;
         }
-        assert.equal(in_foo, "???");
-        assert.equal(this.from_yay, "???");
+        assert.equal(in_foo, "i'm in foo"); // print i'm in foo here 
+        assert.equal(this.from_yay, undefined); // print undefined here
       }
       yay();
       foo();
     });
   
-    QUnit.test("Inner scope variables override outter scope variables.", function(assert){
+    QUnit.test("Inner scope variables override outter scope variables.", function(assert){ // number 6
   
       var peanuts = 300;
   
       function yay(){
         var peanuts = "roasted";
   
-        assert.equal(peanuts, "???");
+        assert.equal(peanuts, "roasted"); // print roasted here
       }
       yay();
   
-      assert.equal(peanuts, "???");
+      assert.equal(peanuts, "300"); // print 300 here
     });
   
-    QUnit.test("Variables created with var in a funtion are re-created each time", function(assert){
+    QUnit.test("Variables created with var in a funtion are re-created each time", function(assert){ // number 7
       function yay(){
         if(this.counter !== undefined){
           counter = counter + 1;
@@ -93,36 +93,38 @@
       }
   
       yay();
-      assert.equal(this.counter, "???");
+      assert.equal(this.counter, undefined); // print undefined here
       yay();
-      assert.equal(this.counter, "???");
+      assert.equal(this.counter, undefined); // print undefined here
       yay();
-      assert.equal(this.counter, "???");
+      assert.equal(this.counter, undefined); // print undefined here
     });
   
-    QUnit.test("Inner scope can access outer scope", function(assert){
+    QUnit.test("Inner scope can access outer scope", function(assert){ //number 8
       var im_outside = "alpha";
       function yay(){
         var im_inside = "omega";
         return im_outside + im_inside;
       }
   
-      assert.equal(yay(), "???");
+      assert.equal(yay(), "alphaomega"); // print alphaomega here
     });
+
   
-    QUnit.test("Functions retain outer scope references between calls.", function(assert){
+    QUnit.test("Functions retain outer scope references between calls.", function(assert){ // num 9
       var im_outside = 13;
       function yay(){
         im_outside += 1;
       }
   
       yay();
-      assert.equal(im_outside, "???");
+      assert.equal(im_outside, "14"); // print 14 here
       yay();
-      assert.equal(im_outside, "???");
+      assert.equal(im_outside, "15"); // print 15 here
     });
+
   
-    QUnit.test("We can do goofy stuff with outer scope", function(assert){
+    QUnit.test("We can do goofy stuff with outer scope", function(assert){ // num 10
   
       var hello = "greg";
       var name = "";
@@ -132,14 +134,14 @@
       }
   
       yay();
-      assert.equal(name, "???");
+      assert.equal(name, "greg"); // print greg here
       yay();
-      assert.equal(name, "???");
+      assert.equal(name, "greggreg"); // print greggreg here
       yay();
-      assert.equal(name, "???");
+      assert.equal(name, "greggreggreg"); // print greggreggreg here
     });
   
-    QUnit.test("We can pass functions to other functions and then run them.", function(assert){
+    QUnit.test("We can pass functions to other functions and then run them.", function(assert){ // num 11
       var im_outter = 10;
       function yay(){
         im_outter /= 5;
@@ -149,18 +151,18 @@
         whatever();
       }
       something(yay);
-      assert.equal(im_outter, "???");
+      assert.equal(im_outter, "???"); // print 
   
     });
   
-    QUnit.test("We can get crazy with returns.", function(assert){
+    QUnit.test("We can get crazy with returns.", function(assert){ // num 12
       function yay(){
         return " is dog";
       }
       function foo(whatever){
         return "hello, this" + whatever();
       }
-      assert.equal(foo(yay), "???");
+      assert.equal(foo(yay), "hello, this is dog"); // print hello, this is dog here
     });
   
   })();
